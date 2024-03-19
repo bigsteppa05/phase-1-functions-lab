b5b0ca8a93744a61c7583e1c41fdd96f394d817f
bigsteppa05 <habibforderiv05@gmail.com>
2024-03-19 11:27:06 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blockNumber) {
+    if (blockNumber > 42) {
+        return blockNumber - 42;
+        
+    } else {
+        return 42 - blockNumber;
+        
+    }
+    
+}
+function distanceFromHqInFeet(blockNumber) {
+    return distanceFromHqInBlocks(blockNumber) * 264;
+    
+}
+function distanceTravelledInFeet(start, destination) {
+    if (start < destination) {
+        return (destination - start) * 264;
+        
+    } else {
+        return (start - destination) * 264;
+        
+    }
+    
+}
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distanceTravelledInFeet(start, destination) <= 400) {
+        return 0;
+        
+    } else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) <= 2000) {
+        return .02 * (distanceTravelledInFeet(start, destination);
+        
+    } else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+        
+    }
+    
+}
